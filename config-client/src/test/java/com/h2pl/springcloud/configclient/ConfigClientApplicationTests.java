e276b97108675f3e77aeab7a227d5712a65d6150
liuwei <wei.liu@shmedo.cn>
2020-07-22 18:58:46 +0800

    SpringCloud Config

---
+++ config-client/src/test/java/com/h2pl/springcloud/configclient/ConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.h2pl.springcloud.configclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
