b46d3bb36626344310c7663c918b375793c2ec84
liuwei <wei.liu@shmedo.cn>
2020-07-22 14:32:43 +0800

    Hystrix for Ribbon

--- ribbon/src/main/java/com/h2pl/springcloud/ribbon/RibbonApplication.java
+++ ribbon/src/main/java/com/h2pl/springcloud/ribbon/RibbonApplication.java
@@ -4,11 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+/**
+ * Ribbon使用断路器
+ */
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableHystrix
 public class RibbonApplication {
 
 	public static void main(String[] args) {
--- ribbon/src/main/java/com/h2pl/springcloud/ribbon/service/HelloService.java
+++ ribbon/src/main/java/com/h2pl/springcloud/ribbon/service/HelloService.java
@@ -1,10 +1,13 @@
 package com.h2pl.springcloud.ribbon.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 /**
+ *
+ * 注解对该方法创建了熔断器的功能，并指定了fallbackMethod熔断方法，熔断方法直接返回了一个字符串
  * Created by Liuwei on 2020/7/22 11:47
  */
 @Service
@@ -12,8 +15,13 @@ public class HelloService {
     @Autowired
     private RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod = "hiError")
     public String helloService(String name) {
         return restTemplate.getForObject("http://SERVICE-HI/hi?name="+name, String.class);
+    }
+
+    public String hiError(String name) {
+        return "hi," + name + ",sorry,the web has a error";
     }
 
 }
