5b5f93dfad54586449da6feb5e2cff6da8f2b77d
liuwei <wei.liu@shmedo.cn>
2020-07-22 18:14:50 +0800

    zuul

---
+++ zuul/src/test/java/com/h2pl/springcloud/zuul/ZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.h2pl.springcloud.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
