46e7b854d98ed13f723ab0fa17ca266bf9efdd36
liuwei <wei.liu@shmedo.cn>
2020-07-23 14:07:04 +0800

    高可用的分布式配置中心

--- eureka-server/src/main/java/com/h2pl/springcloud/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/h2pl/springcloud/eurekaserver/EurekaServerApplication.java
@@ -1,17 +1,15 @@
-package com.h2pl.springcloud.configserver;
+package com.h2pl.springcloud.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableConfigServer
 @EnableEurekaServer
 @SpringBootApplication
-public class ConfigServerApplication {
+public class EurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerApplication.class, args);
+		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 
 }
---
+++ eureka-server/src/test/java/com/h2pl/springcloud/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.h2pl.springcloud.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
